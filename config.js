905964cef9efb4b86beb68c9c44e8b7fe5b3029e
kiki123sgfhj <42903360+kiki123sgfhj@users.noreply.github.com>
2020-06-21 02:07:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NTE3NDIzODc4MzEwNzIzNTk0.XulCTw.jq68Fwcc3OX53RmRuWX7Voy8z5A',    
+    TOKEN: 'NTE3NDIzODc4MzEwNzIzNTk0.Xrgwhw.-PohjxtbuM3NoZ2jZWP3kQolmLU',    
     YT_API_KEY: 'api ', 
     prefix: '$',
     devs: ['517423878310723594']
